6aa488506734496956415866fb2a66d1e42119f8
Ilya Bylich <ibylich@gmail.com>
2015-08-20 15:20:13 +0300

    Small refactoring.

--- lib/binding_dumper/dumpers/abstract.rb
+++ lib/binding_dumper/dumpers/abstract.rb
@@ -1,5 +1,5 @@
 class BindingDumper::Dumpers::Abstract
-  attr_reader :abstract_object, :dumped_ids
+  attr_reader :dumped_ids
 
   def initialize(abstract_object, dumped_ids = [])
     @abstract_object = abstract_object
@@ -7,6 +7,14 @@ class BindingDumper::Dumpers::Abstract
   end
 
   private
+
+  def abstract_object
+    if @abstract_object.is_a?(Hash) && @abstract_object.has_key?(:_object_data)
+      @abstract_object[:_object_data]
+    else
+      @abstract_object
+    end
+  end
 
   def should_convert?
     !dumped_ids.include?(abstract_object.object_id)
--- lib/binding_dumper/universal_dumper.rb
+++ lib/binding_dumper/universal_dumper.rb
@@ -24,15 +24,14 @@ module BindingDumper
     extend self
     extend BindingDumper::Memories
 
-    def converter_for(object)
-      DUMPERS_ON_CONVERTING.detect do |dumper_klass|
-        dumper_klass.new(object).can_convert?
-      end
+    def converter_for(object, dumped_ids)
+      DUMPERS_ON_CONVERTING.map { |dumper_klass|
+        dumper_klass.new(object, dumped_ids)
+      }.detect(&:can_convert?)
     end
 
     def convert(object, dumped_ids = [])
-      converter = converter_for(object)
-      converter.new(object, dumped_ids).convert
+      converter_for(object, dumped_ids).convert
     end
 
     def dump(object)
@@ -41,28 +40,26 @@ module BindingDumper
     end
 
     def deconverter_for(object)
-      DUMPERS_ON_DECONVERTING.detect do |dumper_klass|
-        dumper_klass.new(object).can_deconvert?
+      DUMPERS_ON_DECONVERTING.map do |dumper_klass|
+        dumper_klass.new(object)
+      end.detect do |dumper|
+        dumper.can_deconvert?
       end
     end
 
     def deconvert(converted_data)
-      object_data = if converted_data.is_a?(Hash) && converted_data.has_key?(:_object_data)
-        converted_data[:_object_data]
-      else
-        converted_data
+      deconverter = deconverter_for(converted_data)
+
+      if deconverter.is_a?(Dumpers::PrimitiveDumper)
+        return deconverter.deconvert
       end
 
-      deconverter = deconverter_for(object_data)
-      if deconverter == Dumpers::PrimitiveDumper
-        return deconverter.new(object_data).deconvert
-      end
       old_object_id = converted_data[:_old_object_id]
+
       with_memories(old_object_id) do
-        result = deconverter.new(object_data).deconvert do |object|
+        deconverter.deconvert do |object|
           remember!(object, old_object_id)
         end
-        result
       end
     end
 
